c2fbf9aee0449a92e12e6a7117559ec2397ad382
Neo Chen <neoxen@qq.com>
2014-09-24 14:04:16 +0800

    add comment

--- app/scripts/controllers/posts.js
+++ app/scripts/controllers/posts.js
@@ -10,4 +10,14 @@
 angular.module('yo1App')
     .controller('PostsCtrl', ['$scope', 'posts','$routeParams', function ($scope, posts,$routeParams) {
         $scope.post = posts.posts[$routeParams.id];
+
+        $scope.addComment = function(){
+            if($scope.body === '') { return; }
+            $scope.post.comments.push({
+                body: $scope.body,
+                author: 'user',
+                upvotes: 0
+            });
+            $scope.body = '';
+        };
     }]);
